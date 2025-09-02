201f4e9b459e665228a24b2bb737a06617b663da
victorschmidt21 <naoseivtx21@gmail.com>
2025-09-02 20:48:01 -0300

    config: arrumando config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   build: {
     outDir: "dist",
   },
-  base: "/",
-  plugins: [react()].filter(Boolean),
+  base: "/", // essencial para produção
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
